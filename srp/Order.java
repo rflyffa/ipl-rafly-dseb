242622b128678396674ba5177c5a59d47fcc1941
rflyffa <raflyazrty25@gmail.com>
2023-10-18 08:30:46 +0700

    srp

---
+++ srp/Order.java
@@ -0,0 +1,17 @@
+class Order { 
+ void calculateTotalSum() { 
+ } 
+ 
+ void getItems() { 
+ } 
+ 
+ void getItemCount() { 
+ } 
+ 
+ void addItem(Item item) { 
+ 
+ } 
+ 
+ void deleteItem(Item item) { 
+ } 
+} 
